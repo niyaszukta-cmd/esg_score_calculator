69a4063f316e1228a1aacbbd0c3928693d463bd9
niyaszukta-cmd <niyaszukta@gmail.com>
2025-12-03 04:41:49 +0530

    Update streamlit_esg_dashboard.py

--- streamlit_esg_dashboard.py
+++ streamlit_esg_dashboard.py
@@ -10,7 +10,7 @@
 # streamlit run streamlit_esg_dashboard_complete.py
 #
 # ============================================================================
-
+pip install PyMuPDF pdfplumber
 import streamlit as st
 import pandas as pd
 import numpy as np
